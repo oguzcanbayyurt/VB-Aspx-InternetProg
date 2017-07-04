e6d7e7971cbf40c464d44750ce76b0e87f0fc89d
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-07-05 00:51:55 +0300

    Initial

---
+++ Ogrenci Vize Final Not/Default2.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default2.aspx.vb" Inherits="Default2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        Vize :
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Final :
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Göster" BackColor="Red" 
+            ForeColor="#FFFFCC" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Image ID="Image1" runat="server" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Ogrenci Vize Final Not/Default2.aspx.vb
@@ -0,0 +1,25 @@
+﻿
+Partial Class Default2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
+        Dim vize, final, ortalama As Double
+        vize = TextBox1.Text
+        final = TextBox2.Text
+        ortalama = vize * 4 / 10 + final * 6 / 10
+        If final < 50 Then
+            Label1.Text = "Final Notunuz Düşük,Kaldınız.Ortalamanız = " & ortalama
+            Image1.ImageUrl = "resimler/aglayanyuz.png"
+            Image1.Width = 100
+        ElseIf ortalama >= 60 And final >= 50 Then
+            Label1.Text = "Geçtiniz,Ortalamanız = " & ortalama
+            Image1.ImageUrl = "resimler/gulenyuz.png"
+            Image1.Width = 100
+        Else
+            Label1.Text = "Kaldınız, Ortalamanız = " & ortalama
+            Image1.ImageUrl = "resimler/aglayanyuz.png"
+            Image1.Width = 100
+        End If
+
+    End Sub
+End Class
